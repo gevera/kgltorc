8b9389b26b321efcb3485317d6ef7da7ffa767ec
Denis Donici <denis.donici@mediapark.com>
2022-11-30 13:41:25 +0500

    Added link to external calendar

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -19,4 +19,7 @@
 			scrolling="no"
 		/>
 	</div>
+	<div class="text-center my-4 max-w-lg p-4 mx-auto">
+		<p>In case the calendar is not displayed <a href="https://calendar.google.com/calendar/embed?src=c_93adc9a8597047d74fb6f40aa8e8e5d89426d7cf938de8c1761595412a02702e%40group.calendar.google.com&ctz=America%2FDenver" target="_blank" class="underline text-blue-500 hover:text-blue-800">click here</a> to open it in another window</p>
+		<!-- ><p>In case the calendar is not displayed <a href="https://calendar.google.com/calendar/u/4?cid=Y185M2FkYzlhODU5NzA0N2Q3NGZiNmY0MGFhOGU4ZTVkODk0MjZkN2NmOTM4ZGU4YzE3NjE1OTU0MTJhMDI3MDJlQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20" target="_blank" class="underline">click here</a> to open it in another window</p> -->
 </Section>
