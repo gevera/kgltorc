5149fa7103ee146cb31597f568621610fee3e62f
“gevera” <skryonline@gmail.com>
2023-04-04 15:36:11 +0300

    Updated title in Donate button

--- src/lib/components/DonateButton.svelte
+++ src/lib/components/DonateButton.svelte
@@ -6,7 +6,7 @@
         class="border-none"
 		border="0"
 		name="submit"
-		title="PayPal - The safer, easier way to pay online!"
+		title="Support Karma Gyurmed Ling - Donate via PayPal!"
 		alt="Donate with PayPal button"
 	/>
 	<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
